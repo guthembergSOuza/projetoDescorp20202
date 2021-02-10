48ce175acb9657398a941906ba5da656dc49af75
Gerson Brandão <gerson.leitebrandao@gmail.com>
2021-02-10 08:03:06 -0300

    Adequação ao mapeamento das entidades Contrato, Ator

--- src/main/java/br/com/projetodescorp/model/Ator.java
+++ src/main/java/br/com/projetodescorp/model/Ator.java
@@ -6,13 +6,20 @@
 package br.com.projetodescorp.model;
 
 import java.io.Serializable;
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import org.eclipse.persistence.jpa.config.Cascade;
 
 /**
  *
@@ -31,9 +38,14 @@ public class Ator extends Usuario implements Serializable {
     public Boolean disponivel;
     @Embedded
     public Endereco endereco;
+    @OneToMany(mappedBy = "ator", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    public List<Contrato> contrato;
+
+    
     public Long getId() {
         return id;
     }
+    
 
     public void setId(Long id) {
         this.id = id;
