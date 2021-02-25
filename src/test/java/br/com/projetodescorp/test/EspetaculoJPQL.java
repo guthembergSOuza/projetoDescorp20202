0469c707d11d2b065ee1e8717fc75e882d7ebfa6
guthemberg.souza <guthemberg.souza@orubetecnologia.com.br>
2021-02-25 18:40:57 -0300

    criacao teste espetaculoQuantidadeElenco - ajuste de getData

--- src/test/java/br/com/projetodescorp/test/EspetaculoJPQL.java
+++ src/test/java/br/com/projetodescorp/test/EspetaculoJPQL.java
@@ -5,8 +5,10 @@
  */
 package br.com.projetodescorp.test;
 
+import br.com.projetodescorp.model.Contrato;
 import br.com.projetodescorp.model.Espetaculo;
 import java.util.List;
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
@@ -43,6 +45,32 @@ public class EspetaculoJPQL extends GenericTest {
 
         assertEquals(1, espetaculos.size());
 
+    }
+    
+    @Test
+    public void espetaculoDeDiretoresDistintos() {
+        logger.info("Executando espetaculoDeDiretoresDistintos()");
+        TypedQuery<String> query
+                = em.createQuery("SELECT DISTINCT(e.diretor) FROM Espetaculo e ORDER BY e.diretor", String.class);
+        List<String> diretores = query.getResultList();
+        assertEquals(5, diretores.size());
+    }
+    
+    
+    @Test
+    public void espetaculoQuantidadeElenco() {
+        logger.info("Executando espetaculoQuantidadeElenco()");
+        Query query = em.createQuery("SELECT e, COUNT(c) FROM Espetaculo e, Contrato c WHERE c MEMBER OF e.elenco GROUP BY e");
+        List<Object[]> resultados = query.getResultList();
+        assertEquals(2, resultados.size());
+        assertEquals("O Beijo no Asfalto: 5 Atores", getContratoQuantidade(resultados.get(0)));
+        assertEquals("Geni: 5 Atores", getContratoQuantidade(resultados.get(1)));
+    }
+    
+    private String getContratoQuantidade(Object[] resultado) {
+        Espetaculo esp = (Espetaculo) resultado[0];
+        Long quantidade = (Long) resultado[1];
+        return esp.getTitulo()+ ": " + quantidade + " Atores";
     }
     
 }
