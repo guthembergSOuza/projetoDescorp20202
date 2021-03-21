8b209e13ef6f459f641f1ecd52a177a697de9914
guthemberg.souza <guthemberg.souza@orubetecnologia.com.br>
2021-03-20 22:30:56 -0300

    validacao de Endereco

--- src/test/java/br/com/projetodescorp/test/DiretorValidationTest.java
+++ src/test/java/br/com/projetodescorp/test/DiretorValidationTest.java
@@ -5,28 +5,35 @@ import br.com.projetodescorp.model.TipoDiretor;
 import java.util.Set;
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
+import javax.validation.ConstraintViolationException;
+
 import org.hamcrest.CoreMatchers;
 import static org.hamcrest.CoreMatchers.startsWith;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 
 
 public class DiretorValidationTest extends GenericTest {
     
-    /*
+   /* 
     @Test(expected = ConstraintViolationException.class)
     public void PersistirDiretorInvalido(){
-        Diretor diretor = new Diretor();
+       
         try {
-            diretor.setCompanhiaDeTeatro("Risadaria");
-            diretor.setEmail("alex.siqueira@gmail.com");
-            diretor.setLogin("alex.siqueira");
-            diretor.setNome("Alex Siqueira");
-            diretor.setSenha("@123456@");
+            Diretor diretor = new Diretor();
+            diretor.setNome("Lucimar Junior");
+            diretor.setCompanhiaDeTeatro("Escola de Teatro de Camaragibe");
+            diretor.setEmail("lucimar.junior@gmail.com");
+            diretor.setSenha("11223344");
+            diretor.setLogin("lucimar.junior");
             diretor.setTipoDiretor(TipoDiretor.MUSICAL);
             em.persist(diretor);
             em.flush();
+            
+            assertNotNull(diretor.getId());
+
                     
         } catch (ConstraintViolationException ex) {
             Set<ConstraintViolation<?>> constraintViolations = ex.getConstraintViolations();
@@ -34,7 +41,9 @@ public class DiretorValidationTest extends GenericTest {
             constraintViolations.forEach(violation -> {
                 assertThat(violation.getRootBeanClass() + "." + violation.getPropertyPath() + ": " + violation.getMessage(),
                         CoreMatchers.anyOf(
-                                startsWith("class br.com.projetodescorp.model.Diretor.companhiaDeTeatro = Companhia de Teatro Inválida.")
+                                startsWith("class br.com.projetodescorp.model.Diretor.companhiaDeTeatro: Companhia de Teatro Inválida."),
+                                startsWith("class br.com.projetodescorp.modelDiretor.email: deve ser um endereço de e-mail bem formado")
+
                         )
                 );
             });
